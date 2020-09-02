6b7799badac1255044f6122e9b18d669c8e51fb6
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-02 16:38:38 -0500

    Add sign-in graphql fields

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,5 +1,5 @@
 import merge from 'lodash.merge';
 
-import { listingResolvers } from './Listing';
+import { viewerResolvers } from './Viewer';
 
-export const resolvers = merge(listingResolvers);
+export const resolvers = merge(viewerResolvers);
