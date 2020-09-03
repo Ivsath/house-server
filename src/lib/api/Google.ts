777731271d931bf79b46eaa00f266865e832f705
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-03 10:56:31 -0500

    Set up google sign-in and people api

---
+++ src/lib/api/Google.ts
@@ -0,0 +1,34 @@
+import { google } from 'googleapis';
+
+const auth = new google.auth.OAuth2(
+  process.env.G_CLIENT_ID,
+  process.env.G_CLIENT_SECRET,
+  `${process.env.PUBLIC_URL}/login`,
+);
+
+export const Google = {
+  authUrl: auth.generateAuthUrl({
+    // 'online' (default) or 'offline' (gets refresh_token)
+    access_type: 'online',
+
+    // If you only need one scope you can pass it as a string
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/userinfo.profile',
+    ],
+  }),
+  logIn: async (code: string) => {
+    // This will provide an object with the access_token and refresh_token.
+    // We will not persist tokens because they won't be used at a later time.
+    const { tokens } = await auth.getToken(code);
+
+    auth.setCredentials(tokens);
+
+    const { data } = await google.people({ version: 'v1', auth }).people.get({
+      resourceName: 'people/me',
+      personFields: 'emailAddresses,names,photos',
+    });
+
+    return { user: data };
+  },
+};
