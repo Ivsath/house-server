746f963f5339d5f6b5a1de1d12d6dd5ad9ef2b98
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-07 19:26:58 -0500

    Build the authentication resolvers

--- src/graphql/resolvers/Viewer/index.ts
+++ src/graphql/resolvers/Viewer/index.ts
@@ -1,17 +1,138 @@
 import { IResolvers } from 'apollo-server-express';
+import crypto from 'crypto';
+
+import { Google } from '../../../lib/api';
+import { Database, User, Viewer } from '../../../lib/types';
+import { LogInArgs } from './types';
+
+const logInViaGoogle = async (
+  code: string,
+  token: string,
+  db: Database,
+): Promise<User | undefined> => {
+  const { user } = await Google.logIn(code);
+
+  if (!user) {
+    throw new Error('Google login error');
+  }
+
+  // Name/Photo/Email Lists
+  const userNamesList = user.names && user.names.length ? user.names : null;
+  const userPhotosList = user.photos && user.photos.length ? user.photos : null;
+  const userEmailsList =
+    user.emailAddresses && user.emailAddresses.length
+      ? user.emailAddresses
+      : null;
+
+  // User Display Name
+  const userName = userNamesList ? userNamesList[0].displayName : null;
+
+  // User Id
+  const userId =
+    userNamesList &&
+    userNamesList[0].metadata &&
+    userNamesList[0].metadata.source
+      ? userNamesList[0].metadata.source.id
+      : null;
+
+  // User Avatar
+  const userAvatar =
+    userPhotosList && userPhotosList[0].url ? userPhotosList[0].url : null;
+
+  // User Email
+  const userEmail =
+    userEmailsList && userEmailsList[0].value ? userEmailsList[0].value : null;
+
+  if (!userName || !userId || !userAvatar || !userEmail) {
+    throw new Error('Google login error');
+  }
+
+  const updateRes = await db.users.findOneAndUpdate(
+    { _id: userId },
+    {
+      $set: {
+        name: userName,
+        avatar: userAvatar,
+        contact: userEmail,
+        token,
+      },
+    },
+    { returnOriginal: false },
+  );
+
+  let viewer = updateRes.value;
+
+  if (!viewer) {
+    const insertResult = await db.users.insertOne({
+      _id: userId,
+      token,
+      name: userName,
+      avatar: userAvatar,
+      contact: userEmail,
+      income: 0,
+      bookings: [],
+      listings: [],
+    });
+
+    viewer = insertResult.ops[0];
+  }
+
+  return viewer;
+};
 
 export const viewerResolvers: IResolvers = {
   Query: {
     authUrl: () => {
-      return 'Query.authUrl';
+      try {
+        return Google.authUrl;
+      } catch (err) {
+        throw new Error('Failed to query Google Auth Url: ${error}');
+      }
     },
   },
   Mutation: {
-    logIn: () => {
-      return 'Mutation.logIn';
+    logIn: async (
+      _root: undefined,
+      { input }: LogInArgs,
+      { db }: { db: Database },
+    ): Promise<Viewer> => {
+      try {
+        const code = input ? input.code : null;
+        const token = crypto.randomBytes(16).toString('hex');
+
+        const viewer: User | undefined = code
+          ? await logInViaGoogle(code, token, db)
+          : undefined;
+
+        if (!viewer) {
+          return { didRequest: true };
+        }
+
+        return {
+          _id: viewer._id,
+          token: viewer.token,
+          avatar: viewer.avatar,
+          walletId: viewer.walletId,
+          didRequest: true,
+        };
+      } catch (err) {
+        throw new Error(`Failed to log in: ${err}`);
+      }
     },
-    logOut: () => {
-      return 'Mutation.logOut';
+    logOut: (): Viewer => {
+      try {
+        return { didRequest: true };
+      } catch (err) {
+        throw new Error(`Failed to log out: ${err}`);
+      }
+    },
+  },
+  Viewer: {
+    id: (viewer: Viewer): string | undefined => {
+      return viewer._id;
+    },
+    hasWallet: (viewer: Viewer): boolean | undefined => {
+      return viewer.walletId ? true : undefined;
     },
   },
 };
