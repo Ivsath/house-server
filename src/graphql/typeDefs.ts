d8090e79c916e1e3d46b4593c9c94ef5bc59ddf0
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-07 19:26:00 -0500

    Create schema type defs

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,12 +1,24 @@
 import { gql } from 'apollo-server-express';
 
 export const typeDefs = gql`
+  type Viewer {
+    id: ID
+    token: String
+    avatar: String
+    hasWallet: Boolean
+    didRequest: Boolean!
+  }
+
   type Query {
     authUrl: String!
   }
 
+  input LogInInput {
+    code: String!
+  }
+
   type Mutation {
-    logIn: String!
-    logOut: String!
+    logIn(input: LogInInput): Viewer!
+    logOut: Viewer!
   }
 `;
