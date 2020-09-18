a8d1c5f9806fcbe2442374169f540e372a86ffa6
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-17 20:34:16 -0500

    Create user resolver

--- src/graphql/resolvers/User/index.ts
+++ src/graphql/resolvers/User/index.ts
@@ -1,9 +1,26 @@
 import { IResolvers } from 'apollo-server-express';
 
+import { Database, User } from '../../../lib/types';
+import { UserArgs } from './types';
+
 export const userResolvers: IResolvers = {
   Query: {
-    user: () => {
-      return 'Query.user';
+    user: async (
+      _root: undefined,
+      { id }: UserArgs,
+      { db }: { db: Database },
+    ): Promise<User> => {
+      try {
+        const user = await db.users.findOne({ _id: id });
+
+        if (!user) {
+          throw new Error("User can't be found!");
+        }
+
+        return user;
+      } catch (error) {
+        throw new Error(`Failed to query user: ${error}`);
+      }
     },
   },
 };
