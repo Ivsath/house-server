dc6a00e558c71794b713818cf2335afc4beb2f90
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-17 23:20:39 -0500

    Add user resolvers

--- src/graphql/resolvers/User/index.ts
+++ src/graphql/resolvers/User/index.ts
@@ -3,7 +3,13 @@ import { Request } from 'express';
 
 import { Database, User } from '../../../lib/types';
 import { authorize } from '../../../lib/utils';
-import { UserArgs } from './types';
+import {
+  UserArgs,
+  UserBookingsArgs,
+  UserBookingsData,
+  UserListingsArgs,
+  UserListingsData,
+} from './types';
 
 export const userResolvers: IResolvers = {
   Query: {
@@ -27,6 +33,75 @@ export const userResolvers: IResolvers = {
         return user;
       } catch (error) {
         throw new Error(`Failed to query user: ${error}`);
+      }
+    },
+  },
+  User: {
+    id: (user: User): string => {
+      return user._id;
+    },
+    hasWallet: (user: User): boolean => {
+      return Boolean(user.walletId);
+    },
+    income: (user: User): number | null => {
+      return user.authorized ? user.income : null;
+    },
+    bookings: async (
+      user: User,
+      { limit, page }: UserBookingsArgs,
+      { db }: { db: Database },
+    ): Promise<UserBookingsData | null> => {
+      try {
+        if (!user.authorized) {
+          return null;
+        }
+
+        const data: UserBookingsData = {
+          total: 0,
+          result: [],
+        };
+
+        let cursor = await db.bookings.find({
+          _id: { $in: user.bookings },
+        });
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
+        // page = 1; limit = 10; cursor starts at 0
+        // page = 2; limit = 10; cursor starts at 10
+        cursor = cursor.limit(limit);
+
+        data.total = await cursor.count();
+        data.result = await cursor.toArray();
+
+        return data;
+      } catch (error) {
+        throw new Error(`Failed to query user bookings: ${error}`);
+      }
+    },
+    listings: async (
+      user: User,
+      { limit, page }: UserListingsArgs,
+      { db }: { db: Database },
+    ): Promise<UserListingsData | null> => {
+      try {
+        const data: UserListingsData = {
+          total: 0,
+          result: [],
+        };
+
+        let cursor = await db.listings.find({
+          _id: { $in: user.listings },
+        });
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
+        cursor = cursor.limit(limit);
+
+        data.total = await cursor.count();
+        data.result = await cursor.toArray();
+
+        return data;
+      } catch (error) {
+        throw new Error(`Failed to query user listings: ${error}`);
       }
     },
   },
