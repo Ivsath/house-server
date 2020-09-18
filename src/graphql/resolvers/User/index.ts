b731c04ac224aab6711f8340ab2e5133c55cc2d0
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-17 20:56:36 -0500

    Add authorized flag to the user

--- src/graphql/resolvers/User/index.ts
+++ src/graphql/resolvers/User/index.ts
@@ -1,6 +1,8 @@
 import { IResolvers } from 'apollo-server-express';
+import { Request } from 'express';
 
 import { Database, User } from '../../../lib/types';
+import { authorize } from '../../../lib/utils';
 import { UserArgs } from './types';
 
 export const userResolvers: IResolvers = {
@@ -8,13 +10,18 @@ export const userResolvers: IResolvers = {
     user: async (
       _root: undefined,
       { id }: UserArgs,
-      { db }: { db: Database },
+      { db, req }: { db: Database; req: Request },
     ): Promise<User> => {
       try {
         const user = await db.users.findOne({ _id: id });
 
         if (!user) {
           throw new Error("User can't be found!");
+        }
+
+        const viewer = await authorize(db, req);
+        if (viewer && viewer._id === user._id) {
+          user.authorized = true;
         }
 
         return user;
