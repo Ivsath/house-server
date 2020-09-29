3b3835ac9f04dcb643112c2fbf10011a456b2a47
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-29 11:20:59 -0500

    Create resolver to retrieve a single listing

--- src/graphql/resolvers/Listing/index.ts
+++ src/graphql/resolvers/Listing/index.ts
@@ -1,8 +1,28 @@
 import { IResolvers } from 'apollo-server-express';
+import { ObjectId } from 'mongodb';
 
-import { Listing } from './../../../lib/types';
+import { Database, Listing } from '../../../lib/types';
+import { ListingArgs } from './types';
 
 export const listingResolvers: IResolvers = {
+  Query: {
+    listing: async (
+      _root: undefined,
+      { id }: ListingArgs,
+      { db }: { db: Database },
+    ): Promise<Listing> => {
+      try {
+        const listing = await db.listings.findOne({ _id: new ObjectId(id) });
+        if (!listing) {
+          throw new Error("listing can't be found");
+        }
+
+        return listing;
+      } catch (error) {
+        throw new Error(`Failed to query listing: ${error}`);
+      }
+    },
+  },
   Listing: {
     id: (listing: Listing): string => {
       return listing._id.toString();
