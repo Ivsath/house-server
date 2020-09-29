8254b9144de93e3c5dfa0f807487e7290e946ac3
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-29 11:33:06 -0500

    Check if the listing query is authorized

--- src/graphql/resolvers/Listing/index.ts
+++ src/graphql/resolvers/Listing/index.ts
@@ -1,7 +1,9 @@
 import { IResolvers } from 'apollo-server-express';
+import { Request } from 'express';
 import { ObjectId } from 'mongodb';
 
 import { Database, Listing } from '../../../lib/types';
+import { authorize } from '../../../lib/utils';
 import { ListingArgs } from './types';
 
 export const listingResolvers: IResolvers = {
@@ -9,12 +11,17 @@ export const listingResolvers: IResolvers = {
     listing: async (
       _root: undefined,
       { id }: ListingArgs,
-      { db }: { db: Database },
+      { db, req }: { db: Database; req: Request },
     ): Promise<Listing> => {
       try {
         const listing = await db.listings.findOne({ _id: new ObjectId(id) });
         if (!listing) {
           throw new Error("listing can't be found");
+        }
+
+        const viewer = await authorize(db, req);
+        if (viewer && viewer._id === listing.host) {
+          listing.authorized = true;
         }
 
         return listing;
