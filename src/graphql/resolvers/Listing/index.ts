04df08be60b0bf3d798e9936e1f9c6df8a453f0b
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-30 18:14:47 -0500

    Create the listings type defs and resolvers

--- src/graphql/resolvers/Listing/index.ts
+++ src/graphql/resolvers/Listing/index.ts
@@ -4,7 +4,14 @@ import { ObjectId } from 'mongodb';
 
 import { Database, Listing, User } from '../../../lib/types';
 import { authorize } from '../../../lib/utils';
-import { ListingArgs, ListingBookingsArgs, ListingBookingsData } from './types';
+import {
+  ListingArgs,
+  ListingBookingsArgs,
+  ListingBookingsData,
+  ListingsArgs,
+  ListingsData,
+  ListingsFilter,
+} from './types';
 
 export const listingResolvers: IResolvers = {
   Query: {
@@ -27,6 +34,40 @@ export const listingResolvers: IResolvers = {
         return listing;
       } catch (error) {
         throw new Error(`Failed to query listing: ${error}`);
+      }
+    },
+    listings: async (
+      _root: undefined,
+      { filter, limit, page }: ListingsArgs,
+      { db }: { db: Database },
+    ): Promise<ListingsData> => {
+      try {
+        const data: ListingsData = {
+          total: 0,
+          result: [],
+        };
+
+        let cursor = await db.listings.find({});
+
+        if (filter && filter === ListingsFilter.PRICE_LOW_TO_HIGH) {
+          cursor = cursor.sort({ price: 1 }); // Asc
+        }
+
+        if (filter && filter === ListingsFilter.PRICE_HIGH_TO_LOW) {
+          cursor = cursor.sort({ price: -1 }); // Desc
+        }
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0);
+        // page = 1; limit = 10; cursor starts at 0
+        // page = 2; limit = 10; cursor starts at 10
+        cursor = cursor.limit(limit);
+
+        data.total = await cursor.count();
+        data.result = await cursor.toArray();
+
+        return data;
+      } catch (error) {
+        throw new Error(`Failed to query listings: ${error}`);
       }
     },
   },
