a68b5ef39681ebf80623a22f4a0f674dc61c47e0
Irvin Reyes <reyes.yerp@gmail.com>
2020-10-02 11:21:30 -0500

    Add function to obtain geographic information

--- src/lib/api/Google.ts
+++ src/lib/api/Google.ts
@@ -1,3 +1,9 @@
+import {
+  AddressComponent,
+  AddressType,
+  Client,
+  GeocodingAddressComponentType,
+} from '@googlemaps/google-maps-services-js';
 import { google } from 'googleapis';
 
 const auth = new google.auth.OAuth2(
@@ -5,6 +11,33 @@ const auth = new google.auth.OAuth2(
   process.env.G_CLIENT_SECRET,
   `${process.env.PUBLIC_URL}/login`,
 );
+
+const maps = new Client({});
+
+const parseAddress = (addressComponents: AddressComponent[]) => {
+  let country = null;
+  let admin = null;
+  let city = null;
+
+  for (const component of addressComponents) {
+    if (component.types.includes(AddressType.country)) {
+      country = component.long_name;
+    }
+
+    if (component.types.includes(AddressType.administrative_area_level_1)) {
+      admin = component.long_name;
+    }
+
+    if (
+      component.types.includes(AddressType.locality) ||
+      component.types.includes(GeocodingAddressComponentType.postal_town)
+    ) {
+      city = component.long_name;
+    }
+  }
+
+  return { country, admin, city };
+};
 
 export const Google = {
   authUrl: auth.generateAuthUrl({
@@ -30,5 +63,19 @@ export const Google = {
     });
 
     return { user: data };
+  },
+  geocode: async (address: string) => {
+    if (!process.env.G_GEOCODE_KEY)
+      throw new Error('Missing Google Maps API key');
+
+    const res = await maps.geocode({
+      params: { address, key: process.env.G_GEOCODE_KEY },
+    });
+
+    if (res.status < 200 || res.status > 299) {
+      throw new Error('Failed to geocode address');
+    }
+
+    return parseAddress(res.data.results[0].address_components);
   },
 };
