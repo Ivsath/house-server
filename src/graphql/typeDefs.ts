c74843ce6a3c44c52274cbd364e59f93fcc5d8e4
Irvin Reyes <reyes.yerp@gmail.com>
2020-10-06 23:43:27 -0500

    Add createBooking definition and mutation placeholders

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -102,5 +102,6 @@ export const typeDefs = gql`
     connectStripe(input: ConnectStripeInput!): Viewer!
     disconnectStripe: Viewer!
     hostListing(input: HostListingInput!): Listing!
+    createBooking: String!
   }
 `;
