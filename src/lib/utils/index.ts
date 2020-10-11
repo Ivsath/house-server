4cf44ce843075e5efd66b65b3d170ae9e6a2a238
Irvin Reyes <reyes.yerp@gmail.com>
2020-10-10 23:50:34 -0500

    Update authorize util function

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,16 +1,19 @@
 import { Request } from 'express';
 
-import { Database, User } from '../types';
+import { UserEntity } from '../../database/entity';
+import { Database } from '../types';
 
 export const authorize = async (
   db: Database,
   req: Request,
-): Promise<User | null> => {
+): Promise<UserEntity | null> => {
   const token = req.get('X-CSRF-TOKEN');
   const viewer = await db.users.findOne({
-    _id: req.signedCookies.viewer,
+    id: req.signedCookies.viewer,
     token,
   });
+
+  if (!viewer) return null;
 
   return viewer;
 };
